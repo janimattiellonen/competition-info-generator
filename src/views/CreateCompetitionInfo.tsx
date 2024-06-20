9c0e68661b21f5eb13887c42e6682f14d7ebab88
janimattiellonen <janimatti.ellonen@gmail.com>
2024-06-20 18:59:42 +0300

    dsfgf

--- src/views/CreateCompetitionInfo.tsx
+++ src/views/CreateCompetitionInfo.tsx
@@ -16,6 +16,19 @@ const Row = styled.div`
     }
 `;
 
+const MainFlex = styled.div`
+
+    
+    div:first-of-type {
+        flex-basis: 50%;
+    }
+    
+    @media (min-width: 1200px) {
+        display: flex;
+        flex-direction: row;
+    }
+`;
+
 const Div = styled.div`
     width: 50%;
     display: flex;
@@ -34,6 +47,10 @@ export function CreateCompetitionInfo() {
   const [competitionHost, setCompetitionHost] = useState<string>('puskasoturit');
 
   const generateQR = async (text: string) => {
+    if (!text)  {
+      return;
+    }
+
     try {
       const qrData = await QRCode.toDataURL(text)
 
@@ -44,49 +61,50 @@ export function CreateCompetitionInfo() {
   }
 
   return (
-    <div>
-      <h1>Luo kilpailun infolappunen</h1>
-      <Div>
-        <Row>
-          <label htmlFor="title">Otsikko</label>
-          <input id="title" name="title" value={title} onChange={(e) => setTitle(e.target.value)}/>
-        </Row>
+    <MainFlex>
+      <div>
+        <h1>Luo kilpailun infolappunen</h1>
+        <Div>
+          <Row>
+            <label htmlFor="title">Otsikko</label>
+            <input id="title" name="title" value={title} onChange={(e) => setTitle(e.target.value)}/>
+          </Row>
 
-        <Row>
-          <label htmlFor="date">Päivämäärä</label>
-          <input id="date" name="date" value={date} onChange={(e) => setDate(e.target.value)}/>
-        </Row>
+          <Row>
+            <label htmlFor="date">Päivämäärä</label>
+            <input id="date" name="date" value={date} onChange={(e) => setDate(e.target.value)}/>
+          </Row>
 
-        <Row>
-          <label htmlFor="urlDescription">Kuvaus</label>
-          <input id="urlDescription" name="urlDescription" value={urlDescription} onChange={(e) => setUrlDescription(e.target.value)}/>
-        </Row>
+          <Row>
+            <label htmlFor="urlDescription">Kuvaus</label>
+            <input id="urlDescription" name="urlDescription" value={urlDescription} onChange={(e) => setUrlDescription(e.target.value)}/>
+          </Row>
 
-        <Row>
-          <label htmlFor="content">Sisältö</label>
-          <input id="content" name="content" value={content} onChange={(e) => setContent(e.target.value)}/>
-        </Row>
+          <Row>
+            <label htmlFor="content">Sisältö</label>
+            <input id="content" name="content" value={content} onChange={(e) => setContent(e.target.value)}/>
+          </Row>
 
-        <Row>
-          <label htmlFor="url">Kisan osoite</label>
-          <input id="url" name="url" value={url} onChange={(e) => setUrl(e.target.value)}/>
-        </Row>
-        <Row>
-          <button onClick={() => generateQR(url)}>Luo</button>
-        </Row>
+          <Row>
+            <label htmlFor="url">Kisan osoite* </label>
+            <input id="url" name="url" value={url} onChange={(e) => setUrl(e.target.value)} aria-required aria-invalid={!url} />
+          </Row>
+          <Row>
+            <button disabled={!url} onClick={() => generateQR(url)}>Luo</button>
+          </Row>
 
-        <Row>
-          <label htmlFor="">Kilpailun vetäjä</label>
-          <select name="competitionHost" onChange={(e) => setCompetitionHost(e.target.value)}>
-            <option value="puskasoturit" selected={competitionHost === 'puskasoturit'}>Puskasoturit ry</option>
-            <option value="fribakisat" selected={competitionHost === 'fribakisat'}>Fribakisat.fi</option>
-            <option value="nbdg" selected={competitionHost === 'nbdg'}>NBDG</option>
-          </select>
-        </Row>
-      </Div>
-
+          <Row>
+            <label htmlFor="">Kilpailun vetäjä</label>
+            <select name="competitionHost" onChange={(e) => setCompetitionHost(e.target.value)}>
+              <option value="puskasoturit" selected={competitionHost === 'puskasoturit'}>Puskasoturit ry</option>
+              <option value="fribakisat" selected={competitionHost === 'fribakisat'}>Fribakisat.fi</option>
+              <option value="nbdg" selected={competitionHost === 'nbdg'}>NBDG</option>
+            </select>
+          </Row>
+        </Div>
+      </div>
       {qrCode &&
           <Preview title={title} date={date} description={urlDescription} url={url} content={content} qrCode={qrCode} competitionHost={competitionHost}/>}
-    </div>
+    </MainFlex>
   );
 }
